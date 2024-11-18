ac4ee38046b94c5e7ac0e8c9b2fed009eb485848
MohamedLamineF <fadimoh@hotmail.fr>
2024-11-18 22:41:59 +0100

    init css

--- src/App.tsx
+++ src/App.tsx
@@ -4,8 +4,8 @@ function App() {
   return (
     <>
       <div id="quote-box">
-        <div id="text"></div>
-        <div id="author"></div>
+        <div id="text">Text</div>
+        <div id="author">Quote Author</div>
         <button id="new-quote">New Quote</button>
         <a id="tweet-quote">Tweet</a>
       </div>
