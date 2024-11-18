66290b9dfb442c189e39734f0115be4a2ecf85cb
MohamedLamineF <fadimoh@hotmail.fr>
2024-11-18 23:06:20 +0100

    test

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,31 @@
+import { useState } from "react";
 import "./App.css";
 
+const COLORS = [
+  "bg-indigo-600",
+  "bg-purple-600",
+  "bg-pink-600",
+  "bg-rose-600",
+  "bg-orange-600",
+  "bg-amber-600",
+  "bg-lime-600",
+  "bg-emerald-600",
+  "bg-teal-600",
+  "bg-cyan-600",
+] as const;
 function App() {
+  const [bgColor, setBgColor] = useState<string>(COLORS[0]);
   return (
-    <>
+    <div
+      className={`min-h-screen ${bgColor} transition-colors duration-500 flex items-center justify-center p-4`}
+    >
       <div id="quote-box">
         <div id="text">Text</div>
         <div id="author">Quote Author</div>
         <button id="new-quote">New Quote</button>
         <a id="tweet-quote">Tweet</a>
       </div>
-    </>
+    </div>
   );
 }
 
