b9b6033fac86bd31263b8e9762d6c548d3676440
MohamedLamineF <fadimoh@hotmail.fr>
2024-11-26 07:59:16 +0100

    organize file

---
+++ src/types.ts
@@ -0,0 +1,508 @@
+export interface QuoteData {
+  content: string;
+  author: string;
+}
+
+export const QUOTES: QuoteData[] = [
+  {
+    content:
+      "You will face many defeats in life, but never let yourself be defeated.",
+    author: "Maya Angelou, 'And Still I Rise'",
+  },
+  {
+    content:
+      "In three words I can sum up everything I've learned about life: It goes on.",
+    author: "Robert Frost",
+  },
+  {
+    content: "Life is a long lesson in humility.",
+    author: "J.M. Barrie, 'The Little Minister'",
+  },
+  {
+    content:
+      "To live is the rarest thing in the world. Most people exist, that is all.",
+    author: "Oscar Wilde",
+  },
+  {
+    content:
+      "The most important thing is to enjoy your life–to be happy–it's all that matters.",
+    author: "Audrey Hepburn",
+  },
+  {
+    content:
+      "To succeed in life, you need three things: a wishbone, a backbone and a funnybone.",
+    author: "Reba McEntire",
+  },
+  {
+    content:
+      "We must be willing to let go of the life we planned so as to have the life that is waiting for us.",
+    author: "Joseph Campbell",
+  },
+  {
+    content:
+      "Life is a succession of lessons which must be lived to be understood.",
+    author: "Ralph Waldo Emerson",
+  },
+  {
+    content: "Love the life you live. Live the life you love.",
+    author: "Bob Marley",
+  },
+  {
+    content:
+      "I was taught that the way of progress was neither swift nor easy.",
+    author: "Marie Curie",
+  },
+  {
+    content: "He who has a why to live for can bear almost any how.",
+    author: "Friedrich Nietzsche",
+  },
+  {
+    content: "You only live once, but if you do it right, once is enough.",
+    author: "Mae West",
+  },
+  {
+    content:
+      "The whole secret of a successful life is to find out what is one's destiny to do, and then do it.",
+    author: "Henry Ford",
+  },
+  {
+    content: "In order to write about life first you must live it.",
+    author: "Ernest Hemingway",
+  },
+  {
+    content: "Life has no limitations, except the ones you make.",
+    author: "Les Brown",
+  },
+  { content: "Live for each second without hesitation.", author: "Elton John" },
+  {
+    content: "The most wasted of all days is one without laughter.",
+    author: "E. E. Cummings",
+  },
+  {
+    content: "Start each day with a positive thought and a grateful heart.",
+    author: "Roy Bennett",
+  },
+  {
+    content:
+      "All you need in this life is ignorance and confidence; then success is sure.",
+    author: "Mark Twain",
+  },
+
+  {
+    content:
+      "I have very strong feelings about how you lead your life. You always look ahead, you never look back.",
+    author: "Ann Richards",
+  },
+  {
+    content:
+      "Life is like riding a bicycle. To keep your balance, you must keep moving.",
+    author: "Albert Einstein",
+  },
+  {
+    content: "Life shrinks or expands in proportion to one's courage.",
+    author: "Anais Nin",
+  },
+  {
+    content: "You do not find the happy life. You make it.",
+    author: "Camilla Eyring Kimball",
+  },
+  {
+    content:
+      "A life is not important except in the impact it has on other lives.",
+    author: "Jackie Robinson",
+  },
+
+  {
+    content:
+      "The biggest adventure you can ever take is to live the life of your dreams.",
+    author: "Oprah Winfrey",
+  },
+  {
+    content: "If you own this story you get to write the ending.",
+    author: "Brené Brown",
+  },
+  {
+    content:
+      "Life is like a coin. You can spend it any way you wish, but you only spend it once.",
+    author: "Lillian Dickinson",
+  },
+  {
+    content: "Life is about making an impact, not making an income.",
+    author: "Kevin Kruse",
+  },
+  {
+    content: "There are no regrets in life, just lessons.",
+    author: "Jennifer Aniston",
+  },
+  {
+    content: "Accept no one's definition of your life, define yourself.",
+    author: "Harvey Fierstein",
+  },
+  {
+    content: "The longer I live the more beautiful life becomes.",
+    author: "Frank Lloyd Wright",
+  },
+  {
+    content:
+      "If you love life, don't waste time, for time is what life is made up of.",
+    author: "Bruce Lee",
+  },
+  {
+    content:
+      "Mistakes are a fact of life. It is the response to the error that counts.",
+    author: "Nikki Giovanni",
+  },
+  {
+    content:
+      "Life is not a problem to be solved, but a reality to be experienced.",
+    author: "Soren Kierkegaard",
+  },
+  {
+    content:
+      "Your time is limited, so don't waste it living someone else's life.",
+    author: "Steve Jobs",
+  },
+  {
+    content:
+      "If everything was perfect, you would never learn and you would never grow.",
+    author: "Beyoncé",
+  },
+  {
+    content:
+      "If we don't change, we don't grow. If we don't grow, we aren't really living.",
+    author: "Gail Sheehy",
+  },
+  {
+    content: "Life doesn't have to be perfect to be wonderful.",
+    author: "Annette Funicello",
+  },
+  {
+    content:
+      "The big lesson in life, baby, is never be scared of anyone or anything.",
+    author: "Frank Sinatra",
+  },
+  {
+    content:
+      "I think I've discovered the secret of life – you just hang around until you get used to it.",
+    author: "Charles Schulz",
+  },
+  {
+    content:
+      "Life is very interesting... in the end, some of your greatest pains, become your greatest strengths.",
+    author: "Drew Barrymore",
+  },
+  {
+    content:
+      "Challenges are what make life interesting and overcoming them is what makes life meaningful.",
+    author: "Joshua J. Marine",
+  },
+  {
+    content:
+      "Make it a rule of life never to regret and never to look back. Regret is an appalling waste of energy; you can't build on it; it's only good for wallowing in.",
+    author: "Katherine Mansfield",
+  },
+  {
+    content:
+      "The most important trip you may take in life is meeting people halfway.",
+    author: "Henry Boye",
+  },
+  {
+    content:
+      "Life isn't about finding yourself. Life is about creating yourself.",
+    author: "George Bernard Shaw",
+  },
+  {
+    content:
+      "Fake friends are like shadows: always near you at your brightest moments, but nowhere to be seen at your darkest hour.",
+    author: "Khalil Gibran",
+  },
+  {
+    content:
+      "False friendship, like ivy, decays and ruins the walls it embraces; but true friendship gives new life and animation to the object it supports.",
+    author: "Richard Burton",
+  },
+  {
+    content:
+      "Being betrayed is one of the most valuable lessons life can teach.",
+    author: "Shania Twain",
+  },
+  {
+    content:
+      "Sometimes the person you'd take a bullet for ends up being behind the gun.",
+    author: "Tupac",
+  },
+  {
+    content: "We never lose friends; fake ones are exposed.",
+    author: "Carlos Wallace",
+  },
+  {
+    content:
+      "Time passes and you begin to see people for who they really are and not who they pretend to be.",
+    author: "Scarlet Koop",
+  },
+  {
+    content:
+      "Most people want to see you do better, but not doing better than them.",
+    author: "London Mond",
+  },
+  {
+    content:
+      "It's hard to tell who has your back, from who has it long enough just to stab you in it.",
+    author: "Nicole Richie",
+  },
+  {
+    content: "Best friends has 11 letters but so does backstabber.",
+    author: "Wiz Khalifa",
+  },
+  {
+    content:
+      "No person is your friend who demands your silence, or denies your right to grow.",
+    author: "Alice Walker",
+  },
+  {
+    content: "They're not happy for you because they wish it were them.",
+    author: "Carlos Wallace",
+  },
+  {
+    content: "Make sure the lions you roll with aren't snakes in disguise.",
+    author: "Beenereux Philip",
+  },
+  {
+    content:
+      "Lots of people want to ride with you in the limo, but what you want is someone who will take the bus with you when the limo breaks down.",
+    author: "Oprah Winfrey",
+  },
+  {
+    content:
+      "In the end, we will remember not the words of our enemies, but the silence of our friends.",
+    author: "Martin Luther King Jr.",
+  },
+  {
+    content:
+      "Fake friends are like soap bubbles. They pop out when the sun shines brightly.",
+    author: "Chiranjude Bird",
+  },
+  {
+    content: "Fake friends are with you today and against you tomorrow.",
+    author: "Shizra",
+  },
+  {
+    content:
+      "Sometimes it's not the person who changes, it's the mask that falls off.",
+    author: "Mhaj Porras",
+  },
+  {
+    content:
+      "Stop letting people who do so little for you control so much off your mind, feelings, and emotions.",
+    author: "Will Smith",
+  },
+  {
+    content:
+      "Be very careful of who you share your problem with, remember that not every friend that smiles at you is your best friend.",
+    author: "Kemmy Nola",
+  },
+  {
+    content:
+      "A friend who stands with you in pressure is more valuable than a hundred ones who stand with you in pleasure.",
+    author: "Edward G. Bulwer Lytton",
+  },
+  {
+    content:
+      "You never lose friends. Real ones always stay — no matter what about the fakes, you don't need anyways.",
+    author: "Drishti Bablani",
+  },
+  {
+    content:
+      "You should be aware of fake friend because it is the hardest to be aware of.",
+    author: "Eraldo Banovac",
+  },
+  {
+    content: "Better an honest enemy than a false friend.",
+    author: "German Proverb",
+  },
+  {
+    content:
+      "The worst pain in the world goes beyond the physical. Even further beyond any other emotional pain one can feel. It is the betrayal of a friend.",
+    author: "Heather Brewer",
+  },
+  {
+    content: "What greater wound is there than a false friend?",
+    author: "Sophocles",
+  },
+  {
+    content:
+      "Some people think that the truth can be hidden away with a little cover-up and decoration. But as time goes by, what is true is revealed, and what is fake fades away.",
+    author: "Ismail Haniyeh",
+  },
+  {
+    content: "Friends ask you questions, enemies question you.",
+    author: "Criss Jami",
+  },
+  {
+    content:
+      "A true friend never gets in your way unless you happen to be going down.",
+    author: "Arnold H. Glasow",
+  },
+  {
+    content:
+      "You will never have to question the intentions or integrity of people who have your best interest at heart.",
+    author: "Germany Kent",
+  },
+  {
+    content: "People only throw shade on what's shining.",
+    author: "Genereux Philip",
+  },
+  {
+    content:
+      "Letting go of toxic people in your life is a big step in loving yourself.",
+    author: "Hussein Nishah",
+  },
+  {
+    content:
+      "If you're absent during my struggle, don't expect to be present during my success.",
+    author: "Will Smith",
+  },
+  {
+    content:
+      "You got nothing to lose. You don't lose when you lose fake friends.",
+    author: "Joan Jett",
+  },
+  {
+    content:
+      "At some point you have to learn that some people can stay in your heart but not in your life.",
+    author: "Sandi Lynn",
+  },
+  {
+    content:
+      "You create more space in your life when you turn your excess baggage to garbage.",
+    author: "Chinonye J. Chudolue",
+  },
+  {
+    content: "Never give your attention to someone who deserves your silence.",
+    author: "Emma Xu",
+  },
+  {
+    content:
+      "It makes no sense to try to extend a friendship that was only meant to be a season into a lifetime.",
+    author: "Mandy Hale",
+  },
+  {
+    content:
+      "As he read, I fell in love the way you fall asleep: slowly, and then all at once.",
+    author: "John Green",
+  },
+  {
+    content: "Loved you yesterday, love you still, always have, always will.",
+    author: "Elaine Davis",
+  },
+  {
+    content:
+      "I saw that you were perfect, and so I loved you. Then I saw that you were not perfect and I loved you even more.",
+    author: "Angelita Lim",
+  },
+  {
+    content: "If I know what love is, it is because of you.",
+    author: "Hermann Hesse",
+  },
+  {
+    content:
+      "I swear I couldn’t love you more than I do right now, and yet I know I will tomorrow.",
+    author: "Leo Christopher",
+  },
+  {
+    content:
+      "If you live to be a hundred, I want to live to be a hundred minus one day so I never have to live without you.",
+    author: "A. A. Milne",
+  },
+  {
+    content:
+      "A man is already halfway in love with any woman who listens to him.",
+    author: "Brendan Francis",
+  },
+  {
+    content:
+      "I love you as certain dark things are to be loved, in secret, between the shadow and the soul.",
+    author: "Pablo Neruda",
+  },
+  {
+    content: "Women are meant to be loved, not to be understood.",
+    author: "Oscar Wilde",
+  },
+  { content: "You make me want to be a better man.", author: "Melvin Udall" },
+  {
+    content:
+      "Thinking of you keeps me awake. Dreaming of you keeps me asleep. Being with you keeps me alive.",
+    author: "Unknown",
+  },
+  {
+    content:
+      "Take my hand, take my whole life too. For I can’t help falling in love with you.",
+    author: "Elvis Presley",
+  },
+  {
+    content: "Grow old along with me; the best is yet to be.",
+    author: "Robert Browning",
+  },
+  {
+    content:
+      "The real lover is a man who can thrill you by kissing your forehead or smiling into your eyes or just staring into space.",
+    author: "Marilyn Monroe",
+  },
+  {
+    content:
+      "In all the world, there is no heart for me like yours. In all the world, there is no love for you like mine.",
+    author: "Maya Angelou",
+  },
+  {
+    content: "I’ll be loving you, always with a love that’s true",
+    author: "Patsy Cline",
+  },
+  {
+    content:
+      "Thinking of you keeps me awake. Dreaming of you keeps me asleep. Being with you keeps me alive.",
+    author: "Unknown",
+  },
+  { content: "I need you like a heart needs a beat.", author: "One Republic" },
+  {
+    content:
+      "There is never a time or place for true love. It happens accidentally, in a heartbeat, in a single flashing, throbbing moment.",
+    author: "Sarah Dessen",
+  },
+  {
+    content:
+      "Love is that condition in which the happiness of another person is essential to your own.",
+    author: "Robert A. Heinlein",
+  },
+  {
+    content: "He is not a lover who does not love forever.",
+    author: "Euripides",
+  },
+  { content: "To love is to burn, to be on fire.", author: "Jane Austen" },
+  {
+    content:
+      "Love does not begin and end the way we seem to think it does. Love is a battle, love is a war; love is a growing up.",
+    author: "James Baldwin",
+  },
+  {
+    content:
+      "In the end we discover that to love and let go can be the same thing.",
+    author: "Jack Kornfield",
+  },
+  {
+    content:
+      "Pleasure of love lasts but a moment. Pain of love lasts a lifetime.",
+    author: "Bette Davis",
+  },
+  {
+    content: "Love is so short, forgetting is so long.",
+    author: "Pablo Neruda",
+  },
+  {
+    content: "Love is a thing that is full of cares and fears.",
+    author: "Ovid",
+  },
+  {
+    content: "Love is not love until love’s vulnerable.",
+    author: "Theodore Roethke",
+  },
+];
