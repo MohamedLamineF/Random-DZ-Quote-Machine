598ed344b2696ea2897e83cec18d6b6daf1eddba
MohamedLamineF <fadimoh@hotmail.fr>
2024-11-28 22:00:32 +0100

    adjust animation speed

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,7 @@
 import { useState } from "react";
 import "./App.css";
 import QuoteContainer from "./components/QuoteContainer";
-import { QuoteData, QUOTES } from "./types";
-
-const COLORS = [
-  "bg-gradient-to-b from-green-200 via-teal-400 to-cyan-600",
-  "bg-gradient-to-b from-red-500 via-orange-500 to-yellow-500",
-  "bg-gradient-to-b from-cyan-700 via-blue-500 to-indigo-600",
-  "bg-gradient-to-b from-indigo-500 via-purple-500 to-pink-500",
-  "bg-gradient-to-b from-cyan-400 via-blue-400 to-indigo-400",
-  "bg-gradient-to-b from-yellow-500 via-orange-500 to-red-500",
-  "bg-gradient-to-b from-blue-500 via-cyan-500 to-teal-500",
-  "bg-gradient-to-b from-purple-500 via-indigo-500 to-blue-500",
-  "bg-gradient-to-b from-indigo-500 via-blue-500 to-cyan-500",
-  "bg-gradient-to-b from-gray-500 via-green-500 to-emerald-400",
-  "bg-gradient-to-b from-emerald-500 via-teal-500 to-cyan-500",
-  "bg-gradient-to-b from-gray-300 via-yellow-500 to-amber-400",
-  "bg-gradient-to-b from-yellow-500 via-lime-500 to-green-500",
-  "bg-gradient-to-b from-gray-800 via-blue-700 to-gray-900",
-  "bg-gradient-to-b from-pink-200 via-purple-400 to-indigo-600",
-  "bg-gradient-to-b from-yellow-200 via-lime-400 to-green-600",
-];
+import { COLORS, QuoteData, QUOTES } from "./types";
 
 function App() {
   const [bgColor, setBgColor] = useState<string>(COLORS[0]);
@@ -33,7 +14,7 @@ function App() {
     setTimeout(() => {
       setBgColor(newColor);
       setCircleColor("");
-    }, 900);
+    }, 700);
     setQuote(QUOTES[Math.floor(Math.random() * QUOTES.length)]);
   };
 
